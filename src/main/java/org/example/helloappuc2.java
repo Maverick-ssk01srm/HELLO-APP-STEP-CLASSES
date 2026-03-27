63d889aa66dbda7dd85b6fc8ca75645ba797c6bd
Jarvis001arc <saikrishchen001@gmail.com>
2026-03-27 10:26:16 +0530

    uc2

---
+++ src/main/java/org/example/helloappuc2.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class helloappuc2{
+    public static void main(String[] args){
+        String name=args[0];
+        System.out.println("Hello "+name+"!");
+    }
+}
